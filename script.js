fa5555557e9eb6ae329add47ee0dc95587c4336b
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-12 22:35:14 +0000

    added forLoops

---
+++ script.js
@@ -0,0 +1,12 @@
+const numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
+
+// 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
+
+var forLoop = [];
+
+for (var i=0; i < numbers.length; i ++){
+    let data = String.fromCharCode(numbers[i]);
+    forLoop.push(data);
+    
+}
+console.log("this is forLoop", forLoop.join(""));
